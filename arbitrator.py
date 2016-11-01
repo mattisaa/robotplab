eee0e17d41655375b05827c2cba63549f3370062
BBrekken <brekken.benjamin.aune@gmail.com>
2016-11-01 16:25:46 +0100

    Added arbitrator.py + hjelpekode

---
+++ arbitrator.py
@@ -0,0 +1,29 @@
+
+from bbcon import BBCON
+from random import uniform
+
+class Arbitrator():
+
+    def __init__(self):
+        self.behaviors = None
+
+    def choose_action(self, method=None):
+        self.behaviors = BBCON.active_behaviors
+        if method == 0:
+           return self.stochastic(self.behaviors)
+        else:
+           return self.deterministic(self.behaviors)
+
+    def stochastic(self, behaviors):
+        choiseList = []
+        bot = 0
+        for i in range(len(behaviors)):
+            choiseList.append((bot, behaviors[i].get_value))
+            bot = behaviors[i].get_value
+        choise = uniform(0, max(behaviors.get_value))
+        for i in range(len(choiseList)):
+            if choiseList[i][0] <= choise < choiseList[i][1]:
+                return choiseList[i]
+
+    def deterministic(self, behaviors):
+        return max(behaviors.get_value)
