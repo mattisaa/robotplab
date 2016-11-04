6ffd75fd41888b1f3da73eab35c70d2b3ff1c88b
annettejg <annette.giortz@gmail.com>
2016-11-04 16:28:15 +0100

    oppdatert, ny klasse
    -

--- Behavior.py
+++ Behavior.py
@@ -1,9 +1,10 @@
-
+from BBCON import BBCON
+from sensor import CameraSensob
 
 class Behavior:
 
-    def __init__(self, bbcon):
-        self.bbcon = bbcon
+    def __init__(self):
+        self.bbcon = BBCON()
         self.sensobs = []
         self.motor_recommendations = []  # one rec per motob, all motobs are used by all behaviors
         self.active_flag = False  # True = active, False = inactive
@@ -16,10 +17,18 @@ class Behavior:
     def consider_deactivation(self):
         if self.active_flag:
             # Test om den burde være inactive, if set active_flag til False
+            # Forskjellig for hver behavior
+            if ??:
+                self.active_flag = False
+                self.bbcon.deactivate_behavior(self)
 
     def consider_activation(self):
         if not self.active_flag:
             # Sjekk om burde være active, if set active_flag til True
+            # Forskjellig for hver behavior
+            if ??:
+                self.active_flag = True
+                self.bbcon.activate_behavior(self)
 
     def update_behavior(self):
         self.consider_activation()
@@ -30,3 +39,13 @@ class Behavior:
     def sense_and_act(self):
 
         return match_degree
+
+
+class CameraBehaviour(Behavior):
+
+    def __init__(self):
+        self.camera_sensob = CameraSensob()
+
+    def take_picture(self):
+        color_value = self.camera_sensob.dominant_color_in_picture()
+        if color_value == 'red':
